61e02613cf0b14422e663de4b9bf1eb8f61b4337
Gilles Quodbach <quodbach.gilles@gmail.com>
2022-11-08 18:40:33 +0100

    Recode du dropdown BS pour du perso

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ async function getRecipes() {
     console.log(json)
 }
 getRecipes()
+
+const dropdownCaret = document.querySelector('.caret')
+const dropdownButton = document.querySelector('.dropdown-button')
+const dropdownMenu = document.querySelector('.dropdown-list')
+// target = element that triggered event; currentTarget = element that listens to event
+dropdownButton.addEventListener('mouseover', function (e) {
+    dropdownButton.classList.add('show')
+    dropdownMenu.classList.add('show')
+    dropdownCaret.classList.add('reverse')
+})
+dropdownButton.addEventListener('mouseout', function (e) {
+    dropdownButton.classList.remove('show')
+    dropdownMenu.classList.remove('show')
+    dropdownCaret.classList.remove('reverse')
+})
+dropdownMenu.addEventListener('mouseover', function (e) {
+    dropdownButton.classList.add('show')
+    dropdownMenu.classList.add('show')
+    dropdownCaret.classList.add('reverse')
+})
+dropdownMenu.addEventListener('mouseout', function (e) {
+    dropdownButton.classList.remove('show')
+    dropdownMenu.classList.remove('show')
+    dropdownCaret.classList.remove('reverse')
+})
