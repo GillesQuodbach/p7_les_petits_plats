5ee499a3e43a59f837f438040024c00e4a65ea53
Gilles Quodbach <quodbach.gilles@gmail.com>
2022-11-24 13:11:02 +0100

    recode des cards

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -16,23 +16,15 @@ G.idea/sonarlint/issuestore/8/6/868dc4a7f58b20e019815eb68e4b4ab78be16fad,6\e\6e
 w
 G.idea/sonarlint/issuestore/d/d/dd7b3350c29c49497756c7f32c69a3bed18b1cba,9\d\9db44714da783e82392884aeea0d83f0125570ed
 w
-G.idea/sonarlint/issuestore/f/a/fa288d1472d29beccb489a676f68739ad365fc47,c\d\cdb55fb7b9550cf1c93c03e68dfe3882647e90dc
-w
 G.idea/sonarlint/issuestore/8/e/8e8d88a5308780099ca27040bdf05d7f8257eb96,2\2\224f2d1f04138a4076b10f70bc69063d17627423
 w
-G.idea/sonarlint/issuestore/a/c/ac8032c3e61d4fbfc743770a81690f35b1df03d1,5\f\5f766d7b4aabb2761ba4cedc3bd9dc970f86eb9e
-w
 G.idea/sonarlint/issuestore/6/e/6ecd6000a7b6f4a2884412ff19f74193ed089648,3\8\38464f72ef55196e32fe4dfbcc50f1cda5f4cb5d
-w
-G.idea/sonarlint/issuestore/7/0/7030d0b2f71b999ff89a343de08c414af32fc93a,9\d\9d19eaab4327edc07435a682d68ef383811840e5
 N
 scss/_variables_overrides.scss,5\f\5f05f7d480999ab2e56d59c1d30f1d2cdf56b8f8
 K
 scss/_utilities_custom.scss,4\7\47905bcb68fba0110e4313bdd69068d832fdd99a
 ?
 scss/style.scss,e\f\efa4d8b2a29221ba0bb30281ccccf7ccf2ce193a
-=
-css/style.css,1\2\126b0f8c1ec95fb3a2541745bfc21365b438d668
 A
 package-lock.json,f\a\fa288d1472d29beccb489a676f68739ad365fc47
 <
--- js/Factories/cardFactory.js
+++ js/Factories/cardFactory.js
@@ -1,161 +1,81 @@
-let filteredArray = [];
-//Gallery
-const searchResult = document.querySelector("#recipes-gallery");
+// Input de recherche
 const searchInput = document.querySelector("#main-search-input");
 
-// * Réception des données (recettes)
-// Recipes => fichier recipes.js
-async function getRecipes(data) {
-  return { recettes: [...data] };
-}
+// Container de la galerie
+const cardsGallery = document.querySelector("#recipes-gallery");
 
-// * Function ordonnancement
+// Fonction mise en ordre alphabetique
+
 function orderList(data) {
-  return data.sort((a, b) => {
-    if (a.name.toLowerCase() > b.name.toLowerCase()) {
-      return 1;
-    }
+  const orderedData = data.sort((a, b) => {
     if (a.name.toLowerCase() < b.name.toLowerCase()) {
       return -1;
     }
+    if (a.name.toLowerCase() > b.name.toLowerCase()) {
+      return 1;
+    }
     return 0;
   });
+  return orderedData;
 }
-//Appel fonction
-// * On met les recettes dans l'ordre
-const orderedRecipes = orderList(recipes);
-console.log(orderedRecipes);
 
-// Création du DOM
-async function affichageCard(data) {
-  // Création des cards des recettes
-  const recipesCardsSection = document.getElementById("recipes-gallery");
-  data.forEach((recette) => {
-    const recipesData = appFactory(recette);
-    const cardsDOM = recipesData.getDataDOM();
-    recipesCardsSection.appendChild(cardsDOM);
+//Mise en ordre alphabetique
+orderList(recipes);
+// console.log(recipes)
+console.log(recipes);
+//Fonction de création des cards
+function createCardList(recipesList) {
+  //Tableau de chaques RECETTES" (recipesList)
+  recipesList.forEach((recipe) => {
+    //Destructuring des recette
+    const {
+      appliance,
+      description,
+      id,
+      name,
+      ingredients,
+      serving,
+      time,
+      ustensiles,
+    } = recipe;
+
+    //Récupération du tabelau des ingredients
+    const recipeIngredients = recipe.ingredients;
+
+    //Création du container de la card
+    const listContainer = document.createElement("div");
+    listContainer.setAttribute("id", recipe.id);
+    listContainer.setAttribute("class", "col recipe-card");
+    //Fonction createIngredientList
+
+    const cardListIngredient = `${recipeIngredients
+      .map(
+        (item) => `<li>${item.ingredient} ${item.quantity} ${item.unit}</li>`
+      )
+      .join("")
+      .replace("undefined", "")}`;
+
+    //Création du HTML de la card
+    const cardsHTML = `
+            <div id="${id}" class="card card-body-container">
+            <img src="https://picsum.photos/380/178" class="card-img-top" alt="Recipe image">
+            <div class="card-body">
+            <div class="card-hdr d-flex justify-content-between align-items-center">
+                <h5 class="card-title my-auto">${name}</h5>
+                <img src="img/clock.svg" class="card-clock my-auto" alt="Clock">
+                <p class="card-time fw-bold my-auto">${time} min</p>
+            </div>
+            <div class="card-main d-flex justify-content-between mt-3">
+                <ul> ${cardListIngredient}</ul>
+                <p class="card-recipe col-6">${description}</p>
+            </div>
+            </div>
+            </div>
+        `;
+
+    listContainer.innerHTML = cardsHTML;
+    cardsGallery.appendChild(listContainer);
   });
 }
-
-// Affichage des cards
-async function init(data) {
-  const { recettes } = await getRecipes(data);
-  await affichageCard(recettes);
-}
-// Appel affichage des cards
-init(recipes);
-
-//  FACTORY cards
-function appFactory(data) {
-  const { id, name, time, description, ingredients } = data;
-  const picture = "https://picsum.photos/380/178";
-  const clockIconSRC = "img/clock.svg";
-
-  function getDataDOM() {
-    const cardBootstrapColumn = document.createElement("div");
-    cardBootstrapColumn.setAttribute("id", id);
-    cardBootstrapColumn.setAttribute("class", "col recipe-card");
-    // ? Card container
-    const cardContainer = document.createElement("div");
-    cardContainer.setAttribute("id", id);
-    cardContainer.setAttribute("class", "card card-body-container");
-
-    // * Card recipe image
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
-    img.setAttribute("class", "card-img-top");
-    img.setAttribute("alt", "Recipe image");
-
-    // ? Card body container
-    const cardBody = document.createElement("div");
-    cardBody.setAttribute("class", "card-body");
-
-    // Card header (name + icon + time)
-    const cardHeader = document.createElement("div");
-    cardHeader.setAttribute(
-      "class",
-      "card-hdr d-flex justify-content-between align-items-center"
-    );
-
-    const recipeName = document.createElement("h5");
-    recipeName.setAttribute("class", "card-title my-auto");
-    recipeName.textContent = name;
-
-    const clockIcon = document.createElement("img");
-    clockIcon.setAttribute("src", clockIconSRC);
-    clockIcon.setAttribute("class", "card-clock my-auto");
-    clockIcon.setAttribute("alt", "Clock");
-
-    const recipeTime = document.createElement("p");
-    recipeTime.setAttribute("class", "card-time fw-bold my-auto");
-    recipeTime.textContent = time + " min";
-
-    // Card main content ( ingredients + description )
-    const cardMainContent = document.createElement("div");
-    cardMainContent.setAttribute(
-      "class",
-      "card-main d-flex justify-content-between mt-3"
-    );
-
-    // ? Ingredient list container UL
-    const listIngredient = document.createElement("ul");
-    listIngredient.setAttribute("id", "list");
-    listIngredient.setAttribute("class", "col-6 list-group rounded-0");
-
-    // * Ingredient list LI
-    ingredients.forEach((item) => {
-      const { ingredient = "", quantity = "", unit = "" } = item;
-      // console.log(ingredient);
-      // Ingredients de la card
-      const listIngredientItem = document.createElement("li");
-      listIngredientItem.setAttribute("class", "list-group-item");
-      listIngredientItem.innerHTML = `<b>${ingredient}:</b> ${quantity} ${unit}`;
-      listIngredient.appendChild(listIngredientItem);
-    });
-
-    // * Card description
-    const recipeDescription = document.createElement("p");
-    recipeDescription.setAttribute("class", "card-recipe col-6");
-    recipeDescription.textContent = description;
-
-    // Card creation
-    cardBootstrapColumn.appendChild(cardContainer);
-    cardContainer.appendChild(img);
-    cardContainer.appendChild(cardBody);
-    cardBody.appendChild(cardHeader);
-    cardHeader.appendChild(recipeName);
-    cardHeader.appendChild(clockIcon);
-    cardHeader.appendChild(recipeTime);
-    cardBody.appendChild(cardMainContent);
-    cardMainContent.appendChild(listIngredient);
-    cardMainContent.appendChild(recipeDescription);
-
-    return cardBootstrapColumn;
-  }
-
-  return { id, name, time, description, ingredients, getDataDOM };
-}
-
-// ! écoute de l'input de la recherche
-searchInput.addEventListener("input", filterData);
-
-function filterData(e) {
-  const searchedString = e.target.value.toLowerCase().replace(/\s/g, "");
-  if (searchedString.length > 2) {
-    searchResult.innerHTML = "";
-    const inputValue = searchInput.value;
-    const inputLength = searchInput.value.length;
-    console.log(searchInput.value);
-
-    filteredArray = recipes.filter(
-      ({ name, description, ingredients }) =>
-        name.toLowerCase().includes(searchedString) ||
-        description.toLowerCase().includes(searchedString)
-    );
-    console.log(filteredArray);
-    init(filteredArray);
-  } else {
-    init(orderedRecipes);
-  }
-}
-console.log(filteredArray);
+// Creation de la liste des cartes
+createCardList(recipes);
